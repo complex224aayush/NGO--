550af0e7df27810a1d74300d19f393713415473d
Aayush Raj <126936294+complex224aayush@users.noreply.github.com>
2024-06-12 12:55:30 +0530

    Add files via upload

---
+++ src/component/Home/Home.jsx
@@ -0,0 +1,250 @@
+import React from 'react'
+import "./Home.css";
+
+
+import r13 from "../../Images/l234.jpeg";
+import r14 from "../../Images/l123.jpeg";
+import r15 from "../../Images/cc33.jpeg";
+import r16 from "../../Images/121.jpeg";
+import r17 from "../../Images/131.jpeg";
+import r18 from "../../Images/141.jpeg";
+
+import b10 from "../../Images/h123.jpeg";
+import b12 from "../../Images/h234.jpeg";
+import b18 from "../../Images/h345.jpeg";
+import b19 from "../../Images/b19.jpg";
+import b66 from "../../Images/b66.jpg";
+import b20 from "../../Images/b20.jpg";
+import Photo from '../Photo/Photo';
+
+const Home = () => {
+  return (
+    <div className='top'>
+    <div className='home'>
+    <h2 className='w'>.</h2>
+    <h2 className='w'>.</h2>
+    <h2 className='w'>.</h2>
+        <div className='homecontainer'> 
+        <div className='body2'>
+                    <div className='container'>
+                        <div className='wrapper'>
+                            <div className='wrapper-holder'>
+                            <div id='slider-img-14'></div>
+                            <div id='slider-img-24'></div>
+                            <div id='slider-img-34'></div>
+                            <div id='slider-img-44'></div>
+                            <div id='slider-img-45'></div>
+                          
+                            </div>
+                        </div>
+                        <div className='button-holder'>
+                            <a href='#slider-img-1' className='button'></a>
+                            <a href='#slider-img-2' className='button'></a>
+                            <a href='#slider-img-3' className='button'></a>
+                        </div>
+                    </div>
+                    </div>
+        </div>
+        <h1 className='ad2'> BAL JYOTI FOUN<span className='ad21'>DATION - BODHGAYA ( IND ) </span></h1>
+       
+        <h1 className='greet'>
+            GREE<span className='ad21'>TINGS</span>
+        </h1>
+        <div>
+        <div className='back'>
+        <h3 className='greeting'>
+        Hi there,
+Bal Jyoti Foundation has started a prolific vision for serving Rural areas, helping them to improve 
+their livelihood and assure their sustainment using craft and textile bases. This organization has 
+been founded in February 2019 by Mr. Rupak kumar.
+The foundation consists of young and energetic designers and Engineers, Who has a thought process of implementing their design sense in every aspect of the craft and textile backgrounds. The 
+rural areas, consisting of BPL ( Below Poverty Line ) families are the main target for our idea implementation.
+Serving, helping them and building a sense of leadership is the main satisfaction point of the foundation.
+        </h3>
+        </div>
+        </div>
+     
+            <h1 className='cluss'>
+                CLUS<span className='ad21'>TER</span>  </h1>
+                <h2 className='midd1'>BODH<span className='ad211'>GAYA</span></h2>
+                <div className='bflex'>
+                <div className='clustoo'>
+              <div>
+                <img className='cls11'  src={r16} alt=''/>
+              </div>
+              <div>
+                <img className='cls22' src={r17} alt=''/>
+              </div>
+              <div>
+              <img className='cls33' src={r18} alt=''/></div>
+                    </div>
+                 <div>
+                    
+                    <h3 className='greeting211'>
+                    Celebrating Handloom Elegance in Bodhgaya
+Discover the Artistry of Local Artisans
+At our Cluster , we take pride in curating exquisite handloom products that showcase the rich heritage and craftsmanship of local artisans in Bodhgaya. Nestled in the heart of this historic town, our cluster is dedicated to bringing you the finest rugs and apparel that embody the essence of traditional handloom techniques.
+
+Our Unique Collection
+Rugs:
+Immerse your space in the warmth and charm of our handwoven rugs. Each piece tells a story, skillfully crafted by local artisans who have inherited age-old techniques passed down through generations.
+
+Apparel:
+Our apparel collection is thoughtfully designed for both men and women, featuring a range of ethnic wear that captures the essence of traditional handloom craftsmanship. Explore each piece as a testament to the dedication of our local artisans. Experience the beauty of handloom textiles with our exclusive apparel collection. We prominently use cotton and silk to create garments that blend comfort with sophistication. Our product range caters to both men and women, offering a diverse array of ethnic wear that reflects timeless style.
+                    </h3>
+                  
+        </div>
+        </div>
+    
+                <h2 className='midd2'>LOHAR<span className='ad211'>DAGA</span></h2>
+                <div className='bflex'>
+                <div className='clustoo'>
+                <div>
+                <img className='cls11'  src={r13} alt=''/>
+              </div>
+              <div>
+                <img className='cls22' src={r14} alt=''/>
+              </div>
+              <div>
+              <img className='cls33' src={r15} alt=''/>
+              </div>
+              
+                    </div>
+                    </div>
+               
+                 <div>
+                    <h3 className='greeting2111'>
+                    Lohardaga Rug Unit - Empowering Tribal Women through Craftsmanship
+Weaving Dreams, Transforming Lives
+
+Introducing the Lohardaga Rug Unit, a shining illustration of the potent combination of community backing and masterful craftsmanship. Situated at the heart of Lohardaga, our establishment is dedicated to uplifting local indigenous women who were once engaged in crafting items from palm leaves. Overcoming challenges posed by the unique environment—a bauxite area nestled within a dense forest—where traditional transportation is not feasible, we embarked on a mission to elevate their skills and broaden their horizons. Despite the hurdles, our commitment prevailed, and we successfully achieved our goal, turning adversity into an opportunity for growth and empowerment
+                    </h3>
+        </div>
+        </div>
+
+                <h2 className='midd3'>BARAC<span className='ad211'>HATTI</span></h2>
+                <div className='bflex'>
+                <div className='clustoo'>
+                <div>
+                <img className='cls11'  src={b10} alt=''/>
+              </div>
+              <div>
+                <img className='cls22' src={b12} alt=''/>
+              </div>
+              <div>
+              <img className='cls33' src={b18} alt=''/></div>
+                  
+                    </div>
+                    
+                 <div>
+                    <h3 className='greeting211'>
+                    Welcome to Barachatti-Hahesadi, a thriving community where the ancient craft of bamboo weaving is not just preserved but also elevated through upskilling initiatives. In collaboration with NAWARD (National Association for Welfare and Rural Development), this region has become a hub for the art of bamboo craft, with a focus on empowering local artisans through comprehensive training programs.
+
+Bamboo Craft Upskilling in Barachatti-Hahesadi
+In the picturesque setting of Barachatti-Hahesadi, bamboo craft takes center stage. Local artisans, deeply rooted in the tradition of bamboo weaving, undergo transformative upskilling programs facilitated by NAWARD. These initiatives aim to enhance their expertise, introduce contemporary techniques, and broaden their artistic horizons.
+                    </h3>
+        </div>
+        </div>
+      
+        <div className='blackg'>
+        <div className='ab'>
+            
+            <div className='m'>
+                <div>
+            <h2>1.VISION</h2>
+
+</div>
+<div className='inter'>
+<p>•Implementation of a core line-up for the products
+made by Artisans, to be sold in a good platform.</p>
+<p>•Establish a good livelihood income, which would both
+serve: the Artisans and the Handicraft Sector. </p>
+<p>•Building a sense of Leadership quality in the SHGs (Self
+Help Group), SO that they can run their small scale business
+by their own in their future</p>
+
+            </div>
+            </div>
+            <div className='v'>
+                <div>
+            <h2>2.MISSION</h2>
+
+</div>
+<div className='inter'>
+<p>•Bring back life into the extincting crafts of India.</p>
+<p>•Help at least 300 families each year to rise them from their poverty
+level.</p>
+<p>•Sustain these families with craft bases and let them relive a basic
+exposure to the modern World.</p>
+<p>•Empower women where it is needed. 
+</p>
+</div>
+</div>
+
+      
+        </div>
+        <div className='p'>
+        <div>
+    <h2>3.OBJECTIVE</h2>
+    </div>
+    <div className='interr'>
+   <p> •To provide customers with the type of products they want.</p>
+<p>•Profit Margin for Artisans to be higher as compared to normal wages.</p>
+<p>•Involve rural area in business sector, through intervention
+in textile and Handicraft sector.</p>
+<p>•Women empowerment to be the first priority, i:e to bring
+them forward from the Taboo they face.</p>
+    </div>
+ 
+</div>
+</div>
+<h1 className='testi'>TESTIMONIAL</h1>
+<div className='testimonial'>
+   
+<figure class="snip1157">
+  <blockquote>Calvin: You know sometimes when I'm talking, my words can't keep up with my thoughts... I wonder why we think faster than we speak. Hobbes: Probably so we can think twice.
+    <div class="arrow"></div>
+  </blockquote>
+  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sq-sample3.jpg" alt="sq-sample3" />
+  <div class="author">
+    <h5>Pelican Steve <span> LIttleSnippets.net</span></h5>
+  </div>
+</figure>
+<figure class="snip1157 hover">
+  <blockquote>Thank you. before I begin, I'd like everyone to notice that my report is in a professional, clear plastic binder...When a report looks this good, you know it'll get an A. That's a tip kids. Write it down.
+    <div class="arrow"></div>
+  </blockquote>
+  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sq-sample27.jpg" alt="sq-sample27" />
+  <div class="author">
+    <h5>Max Conversion<span> LIttleSnippets.net</span></h5>
+  </div>
+</figure>
+<figure class="snip1157">
+  <blockquote>My behaviour is addictive functioning in a disease process of toxic co-dependency. I need holistic healing and wellness before I'll accept any responsibility for my actions.
+    <div class="arrow"></div>
+  </blockquote>
+  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sq-sample17.jpg" alt="sq-sample17" />
+  <div class="author">
+    <h5>Eleanor Faint<span> LIttleSnippets.net</span></h5>
+  </div>
+</figure>
+</div>
+        <div className='home15'>
+            <div className='greet1111'>
+        <h1>
+       OUR T<span className='ad21'>EAM</span>
+        </h1>
+        </div>
+    <div className='container1'>
+    <Photo/>
+    </div>
+    </div>
+    </div>
+    
+        
+      
+     
+  )
+}
+
+export default Home
